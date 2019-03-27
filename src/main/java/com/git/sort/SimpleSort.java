92a6e13b9ab8c259e1837e4fc59206a19f3eeeb9
veratum <veratum@163.com>
2019-03-27 21:05:46 +0800

    简单排序01

--- src/main/java/com/git/sort/SimpleSort.java
+++ src/main/java/com/git/sort/SimpleSort.java
@@ -3,8 +3,24 @@ package com.git.sort;
 /**
  * @Created by Administrator
  * @Date 2019/03/27 20:53
+ * >> 简单排序
+ * 常用于取序列中最大最小的几个数时。
+ * (如果每次比较都交换，那么就是交换排序；如果每次比较完一个循环再交换，就是简单选择排序。)
+ * 遍历整个序列，将最小的数放在最前面。
+ * 遍历剩下的序列，将最小的数放在最前面。
+ * 重复第二步，直到只剩下一个数。
  */
 public class SimpleSort {
+	/**
+	 * 代码实现：
+	 * 首先确定循环次数，并且记住当前数字和当前位置。
+	 * 将当前位置后面所有的数与当前数字进行对比，小数赋值给key，并记住小数的位置。
+	 * 比对完成后，将最小的值与第一个数的值交换。
+	 * 重复2、3步。
+	 * @param t 排序数组
+	 * @param <T> 类型
+	 * @return 返回结果
+	 */
 	public static <T extends Comparable<T>> T[] getSort(T[] t) {
 		if (t != null && t.length > 0) {
 			int len = t.length;
